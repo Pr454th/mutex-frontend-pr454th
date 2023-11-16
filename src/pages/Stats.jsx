947e7ddb14d0bd5102e36ab718b64a203581697c
Pragadesh BS <pragadeshbs@protonmail.com>
2023-11-16 19:39:24 +0530

    update stats design

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -20,7 +20,7 @@ const Stats = () => {
 
   return (
     <div className="row container mx-auto">
-      <h1>Statistics</h1>
+      <h1>Event Statistics</h1>
       {loading ? (
         <div className="text-center">
           <img
@@ -32,23 +32,46 @@ const Stats = () => {
         </div>
       ) : (
         <div>
-          <h2>Events</h2>
-          <p>Total no. of events: {events.length}</p>
-          <p>
-            Total no. of registrations:{" "}
-            {events.reduce((acc, event) => acc + event.participants.length, 0)}{" "}
-            teams
-          </p>
-          <p>
-            Average no. of team registrations per event:{" "}
-            {Math.round(
-              events.reduce(
-                (acc, event) => acc + event.participants.length,
-                0
-              ) / events.length
-            )}
-          </p>
-          <table className="table">
+          <div className="row text-center align-items-center justify-content-center me-0 py-4">
+            <div className="col-6">
+              <div className={`mx-auto mb-4 col-6 col-md-4`}>
+                <div className="display-3 fw-bold">17</div>
+                <div>
+                  <span className="fs-6 ms-1">Events</span>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className={`mx-auto mb-4 col-6 col-md-4`}>
+                <div className="display-3 fw-bold">
+                  {events.reduce(
+                    (acc, event) => acc + event.participants.length,
+                    0
+                  )}
+                </div>
+                <div>
+                  <span className="fs-6 ms-1">Teams</span>
+                </div>
+              </div>
+            </div>
+            <div className="col-12">
+              <div className={`mx-auto mb-4 col-6 col-md-4`}>
+                <div className="display-3 fw-bold">
+                  {Math.round(
+                    events.reduce(
+                      (acc, event) => acc + event.participants.length,
+                      0
+                    ) / events.length
+                  )}
+                  +
+                </div>
+                <div>
+                  <span className="fs-6 ms-1">Teams per event</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <table className="table table-striped mb-2">
             <thead>
               <tr>
                 <th>S.No.</th>
@@ -61,7 +84,7 @@ const Stats = () => {
                 <tr key={index}>
                   <td>{index + 1}</td>
                   <td>{event.eventName}</td>
-                  <td>{event.participants.length}</td>
+                  <td>{event.participants.length} teams</td>
                 </tr>
               ))}
             </tbody>
