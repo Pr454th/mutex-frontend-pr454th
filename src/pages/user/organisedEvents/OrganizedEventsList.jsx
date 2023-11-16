e8b3afd846dc46dfcfd7f523ec3e4c26914f6ccd
Pragadesh BS <pragadeshbs@protonmail.com>
2023-11-16 09:30:25 +0530

    anchor organized events card

--- src/pages/user/organisedEvents/OrganizedEventsList.jsx
+++ src/pages/user/organisedEvents/OrganizedEventsList.jsx
@@ -14,33 +14,39 @@ const OrganizedEventsList = ({ events }) => {
               style={{ width: "18rem" }}
               key={event._id}
             >
-              <img
-                src={
-                  event.image
-                    ? `${config.apiUrl}/api/events/image/${event._id}`
-                    : image1
-                }
-                className="card-img-top"
-                alt="..."
-              />
-              <div className="card-body">
-                <h5 className="card-title">{event.eventName}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">
-                  {format(
-                    new Date(event.eventStartDate.substr(0, 16)),
-                    "dd MMM yyyy-h:mm a"
-                  )}
-                </h6>
-                <p className="card-text">Venue: {event.venue}</p>
-                <Link to={`/eventdetails/${event._id}`}>
+              <Link
+                to={`/eventdetails/${event._id}`}
+                style={{
+                  textDecoration: "none",
+                  color: "black",
+                }}
+              >
+                <img
+                  src={
+                    event.image
+                      ? `${config.apiUrl}/api/events/image/${event._id}`
+                      : image1
+                  }
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{event.eventName}</h5>
+                  <h6 className="card-subtitle mb-2 text-muted">
+                    {format(
+                      new Date(event.eventStartDate.substr(0, 16)),
+                      "dd MMM yyyy-h:mm a"
+                    )}
+                  </h6>
+                  <p className="card-text">Venue: {event.venue}</p>
                   <button className="btn btn-success">View Event</button>
-                </Link>
-                <Link to={`/events/update/${event._id}`}>
-                  <button className="btn btn-secondary m-1">
-                    Update Event
-                  </button>
-                </Link>
-              </div>
+                  <Link to={`/events/update/${event._id}`}>
+                    <button className="btn btn-secondary m-1">
+                      Update Event
+                    </button>
+                  </Link>
+                </div>
+              </Link>
             </div>
           );
         })}
